2f8183f5fc9b92408c37adcafc3465fe43019c69
Abdimalik11 <ilkacaze5522@gmail.com>
2023-05-30 18:41:05 -0700

    this is the Answer of tip calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const bill = 300;
+
+
+if (bill >= 50 && bill <= 300) {
+ 
+  const tip = bill * 0.15;
+  
+  const total = bill + tip;
+  
+ 
+  console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+} else {
+  
+  const tip = bill * 0.2;
+
+  const total = bill + tip;
+  
+
+  console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+}
